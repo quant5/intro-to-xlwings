1a6db8871f5ed80902fdad83b53b55904921eec5
Kevin Zhu <kzhu@bamfunds.com>
2017-08-02 11:36:51 -0400

    full writeup and excel template finished

--- myproject.py
+++ myproject.py
@@ -1,12 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 import xlwings as xw
+import os
+
+db_uri = os.environ.get('DB_URI', 'sqlite://')
+query = '''
+select year, quarter, metric_value from company_metric
+    where ticker = '{}'
+    order by year, quarter;
+'''
 
 
-def world():
+def hello_world():
     wb = xw.Book.caller()
-    wb.sheets[0].range('A1').value = 'Hello World!'
+    wb.sheets.active.range('A1').value = 'Hello World!'
+
+
+def get_metric():
+    sht = xw.Book.caller().sheets['database']
+    ticker = sht.range('B1').value
+    df = pd.read_sql(query.format(ticker), con=db_uri)
+    sht.range('A4').expand().clear_contents()
+    sht.range('A4').value = df.values
 
 
 @xw.func
 def double_sum(x, y):
     """Return twice the sum of the two arguments"""
     return 2 * (x + y)
+
+
+@xw.func
+def myplot(n):
+    """Sample code to plot a specified range"""
+    sht = xw.Book.caller().sheets.active
+    fig = plt.figure()
+    plt.plot(range(int(n)))
+    sht.pictures.add(fig, name='MyPlot', left=500, top=0, update=True)
+    return 'Plotted with n={}'.format(n)
